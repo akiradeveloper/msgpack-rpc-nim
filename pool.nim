d330e5d3a18cf899b44b88da086b54baa7de0d95
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-16 17:31:59 +0900

    update pool
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- pool.nim
+++ pool.nim
@@ -6,9 +6,9 @@ import asyncnet
 import msgpack
 import msgpack_rpc
 import locks
-import queues
 
-type AllocFn = proc (): AsyncSocket
+type AllocFn* = proc (): AsyncSocket
+
 type Pool = ref object
   lock: TLock
   free: seq[AsyncSocket]
@@ -21,25 +21,28 @@ proc mkPool(f: AllocFn): Pool =
     used: newSeq[AsyncSocket](),
     allocFn: f
   )
-  # FIXME deinitLock on freeing (destructor?)
   initLock(result.lock)
 
+proc destroy(pool: Pool) {.override.} =
+  assert(len(pool.used) == 0)
+  deinitLock(pool.lock)
+  for sock in pool.free:
+    sock.close()
+  
 proc size(pool: var Pool): int =
   pool.lock.acquire
   result = len(pool.free) + len(pool.used)
   pool.lock.release
 
-type MultiFuture = ref object
-  futs: seq[Future[Msg]]
-  pool: Pool
- 
 proc acquire(pool: var Pool): AsyncSocket =
   pool.lock.acquire
-  if len(pool.free) > 0:
-    let i = high(pool.free)
-    result = pool.free[i]
-    pool.free.delete(i)
-    pool.used.add(result)
+  if len(pool.free) == 0:
+    pool.free.add(pool.allocFn())
+  assert(len(pool.free) > 0)
+  let i = high(pool.free)
+  result = pool.free[i]
+  pool.free.delete(i)
+  pool.used.add(result)
   pool.lock.release
  
 proc release(pool: var Pool, sock: AsyncSocket) =
@@ -49,18 +52,35 @@ proc release(pool: var Pool, sock: AsyncSocket) =
   pool.free.add(sock)
   pool.lock.release
 
-proc mkMultiFuture*(f: proc (): AsyncSocket): MultiFuture =
-  discard
+type MultiFuture* = ref object
+  futs: seq[Future[Msg]]
+  finFuts: seq[Msg]
+  pool: Pool
 
-proc join(mfut: MultiFuture): seq[Msg] =
-  for fut in mfut.futs:
-    result.add(waitFor(fut))
+proc mkMultiFuture*(f: AllocFn): MultiFuture =
+  result = MultiFuture (
+    futs: newSeq[Future[Msg]](),
+    finFuts: newSeq[Msg](),
+    pool: mkPool(f)
+  )
 
-proc add(mfut: MultiFuture, name: string, args: seq[Msg]) =
+proc add*(mfut: MultiFuture, name: string, args: seq[Msg]) =
   var mfut = mfut # without this, "illegal capture" error
   let sock = mfut.pool.acquire()
   let cli = mkClient(sock)
   var fut = cli.call(sock.getFd().int, name, args)
   fut.callback = proc () =
     mfut.pool.release(sock)
+    mfut.finFuts.add(fut.read)
   mfut.futs.add(fut)
+
+iterator eachResult*(mfut: MultiFuture): Msg =
+  var i = 0
+  while (len(mfut.finFuts) > i and i < len(mfut.futs)):
+    yield mfut.finFuts[i]
+    i += 1
+
+proc join*(mfut: MultiFuture): seq[Msg] =
+  for fut in mfut.futs:
+    discard waitFor(fut)
+  mfut.finFuts
